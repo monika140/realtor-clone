9e68d4729cd3fdf799bd06c64053a2fbed63af77
Monika <monikadl140@gmail.com>
2023-09-23 17:53:10 +0530

    new changes

--- src/components/OAuth.js
+++ src/components/OAuth.js
@@ -2,18 +2,39 @@ import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { toast } from "react-toastify";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-const onGoogleClick = async () => {
-  try {
-    const auth = getAuth();
-    const provider = new GoogleAuthProvider();
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-  } catch (error) {
-    toast.error("Could not authorize with Google");
-  }
-};
+import { db } from "../firebase";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
-function OAuth() {
+const OAuth = () => {
+  const navigate = useNavigate();
+
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      console.log("hello", user);
+
+      //check for user
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+      //console.log(error);
+    }
+  };
+
   return (
     <button
       onClick={onGoogleClick}
@@ -23,6 +44,6 @@ function OAuth() {
       Continue with google
     </button>
   );
-}
+};
 
 export default OAuth;
